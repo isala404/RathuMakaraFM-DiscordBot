db521b6c4dcccd58bda7501be214402cd9de632e
Isala Piyarisi <isalap99@gmail.com>
2018-10-18 22:21:18 +0530

    Update bot_constants.py

--- bot_constants.py
+++ bot_constants.py
@@ -4,5 +4,7 @@ listening_channels = []
 bot_voice_channel = 0
 player_channel = 0
 bot_cmd_channel = 0
+song_request_channel = 0
+song_request_queue_channel = 0
 auth_key = ''
 ############################################
