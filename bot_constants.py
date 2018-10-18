2288a226e14856f67358620ada51ebfdcc6367e3
Isala Piyarisi <isalap99@gmail.com>
2018-10-18 13:00:14 +0530

    Create bot_constants.py

---
+++ bot_constants.py
@@ -0,0 +1,8 @@
+############################################
+prefix = '!'
+listening_channels = []
+bot_voice_channel = 0
+player_channel = 0
+bot_cmd_channel = 0
+auth_key = ''
+############################################
